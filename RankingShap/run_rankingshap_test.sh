62be1e399cfb9097cdb2813afad7e266e2ef7cc3
Owen de Jong <O.K.dejong@student.tudelft.nl>
2025-11-24 04:03:08 +0100

    Updates to .sh script and removing test statements

--- RankingShap/run_rankingshap_test.sh
+++ RankingShap/run_rankingshap_test.sh
@@ -9,4 +9,4 @@ mkdir results/results_MSLR-WEB10K/feature_attributes
 python3 train_model.py --file_name test_model --dataset MQ2008 --model_type listwise
 python3 generate_feature_attribution_explanations.py --model_file test_model --dataset MQ2008 --experiment_iteration 1 --test
 python3 estimate_ground_truth_attribution_values.py --model_file test_model --dataset MQ2008 --background_samples 100 --nsamples 10 --experiment_iterations 3 --test
-python3 evaluate_feature_attribution_with_ground_truth.py --dataset MQ2008 --file_name_ground_truth feature_importance_approxiation__backgroundcount_100_nsamples_1024_means_test.csv --test
+python3 evaluate_feature_attribution_with_ground_truth.py --model_file test_model --dataset MQ2008 --file_name_ground_truth feature_importance_approxiation__backgroundcount_100_nsamples_1024_means_test.csv --test
