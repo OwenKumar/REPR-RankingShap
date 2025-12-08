17d14b48bde132339ebe40d0240f8b3641154f79
Owen de Jong <O.K.dejong@student.tudelft.nl>
2025-12-08 17:49:28 +0100

    Add random seed based on time in random explainer.

--- RankingShap/approaches/random_explainer.py
+++ RankingShap/approaches/random_explainer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils.explanation import AttributionExplanation
+from datetime import datetime
 
 
 class RandomExplainer:
@@ -9,12 +10,15 @@ class RandomExplainer:
         self.num_features = num_features
 
     def get_query_explanation(self, query_features, query_id=""):
+        np.random.set_state(int(datetime.now().timestamp()))
         explanation = np.random.random(self.num_features)
         explanation = list(explanation / sum(explanation))
         explanation = [(i + 1, explanation[i]) for i in range(self.num_features)]
 
         feature_attributes = AttributionExplanation(
-            explanation=explanation, num_features=self.num_features, query_id=query_id
+            explanation=explanation, 
+            num_features=self.num_features, 
+            query_id=query_id
         )
         feature_selection = feature_attributes.get_top_k_feature_selection(
             self.explanation_size
